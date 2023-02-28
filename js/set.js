0c08ca8579c2d9939964e10a328ca22cfa57830f
Metastem <110768529+Metastem@users.noreply.github.com>
2023-02-28 23:25:51 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -167,7 +167,7 @@ function keywordReminder() {
     var keyword = $(".wd").val();
     if (keyword != "") {
         $.ajax({
-            url: 'https://suggestion.baidu.com/su?wd=' + keyword,
+            url: 'http://suggestqueries.google.com/complete/search?q=' + keyword,
             dataType: 'jsonp',
             jsonp: 'cb', //回调函数的参数名(键值)key
             success: function (data) {
